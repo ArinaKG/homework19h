9c142cff901d48614e83e207f294b5be6ab5b068
Арина <a.kornishina@mail.ru>
2023-02-22 01:44:39 +0300

    try1

---
+++ service/auth.py
@@ -0,0 +1,47 @@
+import jwt
+import calendar
+import datetime
+
+from flask import abort
+
+from helpers.constants import SECRET, ALGO
+from service.user import UserService
+
+
+class AuthService:
+
+    def __init__(self, user_service: UserService):
+        self.user_service = user_service
+
+    def generate_token(self, username, password, is_refresh = False):
+        user = self.user_service.get_by_uername(username)
+
+        if user is None:
+            abort(400)
+        if not is_refresh:
+            if not self.user_service.comprare_password(user.password, password)
+                abort(400)
+
+        data = {
+            'username': username,
+            'role': user.role,
+        }
+
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, SECRET, algorithm=ALGO)
+
+        days30 = datetime.datetime.utcnow() + datetime.timedelta(days=30)
+        data["exp"] = calendar.timegm(days30.timetuple())
+        refresh_token = jwt.encode(data, SECRET, algorithm=ALGO)
+
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token
+        }
+
+    def refresh_token(self, token):
+        data = jwt.decode(jwt=token, key=SECRET, algorithms=ALGO)
+        username = data.get('username')
+
+        return self.generate_token(username, None, is_refresh=True)
